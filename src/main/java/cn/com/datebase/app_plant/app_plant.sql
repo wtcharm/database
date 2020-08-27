933a76e3daf9d2b4aa2a0f23295b65db0b724adf
handsome <handsome@163.com>
2020-08-27 17:17:41 +0800

    添加种花app

--- src/main/java/cn/com/datebase/app_plant/app_plant.sql
+++ src/main/java/cn/com/datebase/app_plant/app_plant.sql
@@ -4,7 +4,10 @@ SET SESSION FOREIGN_KEY_CHECKS=0;
 
 DROP TABLE IF EXISTS app_info;
 DROP TABLE IF EXISTS app_user;
+DROP TABLE IF EXISTS app_user_order;
 DROP TABLE IF EXISTS app_user_site;
+DROP TABLE IF EXISTS user_site_invite;
+DROP TABLE IF EXISTS user_site_urgency;
 
 
 
@@ -28,6 +31,9 @@ CREATE TABLE app_user
 (
 	id int(11) NOT NULL COMMENT '主键',
 	user_phone varchar(50) NOT NULL COMMENT '用户手机号',
+	user_image varchar(50) NOT NULL COMMENT '用户头像',
+	user_invite varchar(20) NOT NULL COMMENT '用户邀请码',
+	user_name varchar(50) NOT NULL COMMENT '用户昵称',
 	app_id int(11) NOT NULL COMMENT 'appId',
 	idfa varchar(50) NOT NULL COMMENT 'idfa',
 	udid varchar(50) NOT NULL COMMENT 'udid',
@@ -39,6 +45,21 @@ CREATE TABLE app_user
 ) COMMENT = '应用用户';
 
 
+-- 用户订单
+CREATE TABLE app_user_order
+(
+	id int(11) NOT NULL COMMENT '主键',
+	app_id int(11) NOT NULL COMMENT 'appId',
+	user_id int(11) NOT NULL COMMENT '用户Id',
+	user_money decimal(10,4) NOT NULL COMMENT '用户金额',
+	apple_voucher varchar(100) NOT NULL COMMENT '苹果凭证',
+	outgoings_type varchar(50) NOT NULL COMMENT '购买类型',
+	create_time datetime NOT NULL COMMENT '创建时间',
+	update_time datetime NOT NULL COMMENT '修改时间',
+	PRIMARY KEY (id)
+) COMMENT = '用户订单';
+
+
 -- 用户的地理位置
 CREATE TABLE app_user_site
 (
@@ -46,10 +67,39 @@ CREATE TABLE app_user_site
 	app_id int(11) NOT NULL COMMENT 'appId',
 	user_id int(11) NOT NULL COMMENT '用户Id',
 	user_site varchar(100) NOT NULL COMMENT '用户位置',
+	outgoings_type varchar(50) NOT NULL COMMENT '购买类型',
+	outgoings_start_time datetime NOT NULL COMMENT '开始时间',
+	outgoings_end_time datetime NOT NULL COMMENT '消费结束时间',
 	create_time datetime NOT NULL COMMENT '创建时间',
 	update_time datetime NOT NULL COMMENT '修改时间',
 	PRIMARY KEY (id)
 ) COMMENT = '用户的地理位置';
+
+
+-- 邀请记录
+CREATE TABLE user_site_invite
+(
+	id int(11) NOT NULL COMMENT '主键',
+	app_id int(11) NOT NULL COMMENT 'appId',
+	parent_invite_id int(11) NOT NULL COMMENT '父级邀请人Id',
+	son_invite_id int(11) NOT NULL COMMENT '子级邀请Id',
+	invite_stauts int(5) NOT NULL COMMENT '邀请状态(200-邀请中,201-失败,210-成功)',
+	create_time datetime NOT NULL COMMENT '创建时间',
+	update_time datetime NOT NULL COMMENT '修改时间',
+	PRIMARY KEY (id)
+) COMMENT = '邀请记录';
+
+
+-- 紧急联系人
+CREATE TABLE user_site_urgency
+(
+	id int(11) NOT NULL COMMENT '主键',
+	user_id int(11) NOT NULL COMMENT '用户Id',
+	urgency_phone varchar(50) NOT NULL COMMENT '紧急联系人',
+	create_time datetime NOT NULL COMMENT '创建时间',
+	update_time datetime NOT NULL COMMENT '修改时间',
+	PRIMARY KEY (id)
+) COMMENT = '紧急联系人';
 
 
 
